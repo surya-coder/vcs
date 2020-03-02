aea909ae3b45ae29cf88f0c8ddbb85eee507745c
surya <sherlockholmesmystery7@gmail.com>
2020-03-02 23:14:01 +0530

    read employee data from the kb as a dynamic data using input function

--- math module.py
+++ math module.py
@@ -8,3 +8,16 @@ import math as m
 print(m.sqrt(12))
 #we can import particuler member as below follows
 from math import sqrt
+#if importing specific meber no need to write math.pi like that  if we are dooing like that we get name error
+#enter the employee details as a dynamic data from the keyboard
+eno=int(input("enter employee number:"))
+ename=input("enter employee name:")
+esal=int(input("Enter the employee salary:"))
+eadder=input("Enter the employee address:")
+married=input("Employee married?[True | False]")
+print("please conform the details:")
+print("Employee Number:",eno)
+print("Employee Name:",ename)
+print("Employee salary:",esal)
+print("Employee address:",eadder)
+print("Employee married:",married)
