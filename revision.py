6a1ec0967c7c79d6f85427ae35af3baf65557ad1
surya <sherlockholmesmystery7@gmail.com>
2020-03-02 22:08:52 +0530

    using ternary opearator accepting values through cli

--- revision.py
+++ revision.py
@@ -3,3 +3,9 @@
 a,b=10,20
 x=30 if a<b else b
 print(x)
+#####
+a=int(input("Enter the first number:"))
+b=int(input("Enter second nummber:"))
+min=a if a<b else b
+print(min)
+# nesting of ternary opearator is possibe.
