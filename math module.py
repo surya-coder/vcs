9691794dd347401deb22dd6448ee87e619f06790
surya <sherlockholmesmystery7@gmail.com>
2020-03-02 22:50:48 +0530

    math module

---
+++ math module.py
@@ -0,0 +1,10 @@
+# math module
+#a module is a collection of functions , varieables , classes etc
+#math module that contain several matchmatical functions to perform on it
+import math
+print(math.sqrt(12))
+# we can aliasing using as keyword.
+import math as m
+print(m.sqrt(12))
+#we can import particuler member as below follows
+from math import sqrt
