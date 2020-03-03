1a377eb950bb485402a08a4ea10491534071abd6
surya <sherlockholmesmystery7@gmail.com>
2020-03-03 10:56:34 +0530

    end of print formating statements

--- revision2.py
+++ revision2.py
@@ -19,3 +19,21 @@ for x in argv:
 #4)with end attribute , by default end attribute is \n if we want then we can use sep attribute
 #5)print object statement we can pass any object like(list,tuple,set,etc)
 #6)print(formated string)####   %i,%d,%f,%s  print("formated string:" %(varieable list))
+a=10
+b=20
+c=30
+print("a value is %i" %a)
+print("b value is %d" %b)
+
+###########
+s="surya"
+list=[10,20,30,40]
+print("Hello %s .... The list of arguments are %s" %(s,list))
+#7)print with replacement opearator
+name='surya'
+salary=25000
+gf='no GF'
+print("hello {0} Your salary is {1} and Your {2} is waiting for you ".format(name,salary,gf))
+
+
+
