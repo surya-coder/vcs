ec353b5fe1332469d6709da53ee7a1ef17e76df1
surya <sherlockholmesmystery7@gmail.com>
2020-03-03 21:24:19 +0530

    for loop executing index wise

---
+++ flowcontrol.py
@@ -0,0 +1,8 @@
+#Iterative statements
+#if we want to execute a group of statements multiple times then we should go for iterative statements
+# for loop if we want to execute some action for every element in the sequence(it may be string or collection) then we should go for  iterative for loop
+s="no one love solder untill enemy is at the border"
+i=0
+for x in s:
+    print("the charcter present at",i,"index is:",x)
+    i=i+1
