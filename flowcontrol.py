0f55dc53af0c8a816cfac2fbef387fb0c486d76f
surya <sherlockholmesmystery7@gmail.com>
2020-03-03 22:20:55 +0530

    end of while loop

--- flowcontrol.py
+++ flowcontrol.py
@@ -22,5 +22,17 @@ sum=0
 for x in d:
     print("the sum is",sum+x)
     sum=sum+x
+#while loop if we want to execute a group of statements iteratively untill some condition false then we should go for while loop
+x=1
+while x<=10:
+    print(x)
+    x=x+1
 
-
+#to display sum of first n nummbers
+n=int(input("Enter number:"))
+sum=0
+i=1
+while i<=n:
+    sum=sum+i
+    i=i+1
+print("sum of",n,"numbers is:",sum)
